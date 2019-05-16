662c830b4519b22b235dc4b6fe77081aaa420cea
Yoann Pradat <pradaty@Yoanns-MacBook-Pro.local>
2019-05-16 22:52:54 +0100

    XGBoost script

--- mortgage/source_2/run_models/run_xgb.py
+++ mortgage/source_2/run_models/run_xgb.py
@@ -31,6 +31,7 @@ parser.add_argument('--n_classes', type=int, default=2, help='number of classes'
 parser.add_argument('--other_lim', type=float, default=0.005, help='threshold for categories gathering')
 parser.add_argument('--model_type', type=str, default='XGB_3',  help='type of model')
 parser.add_argument('--max_depth', type=int, default=10,  help='max_depth')
+parser.add_argument('--eta', type=float, default=0.4,  help='eta')
 parser.add_argument('--colsample_bytree', type=int, default=0.8,  help='colsample_bytree')
 parser.add_argument('--subsample', type=float, default=0.8,  help='subsample')
 parser.add_argument('--random_state', type=int, default=0, help='random state for the model')
@@ -147,8 +148,8 @@ strat_cv = mortgage_data.strat_cv
 
 # ========================== THE MODEL ========================== #
 param_grid = {'booster': ['gbtree'],
-              'min_child_weight': [10, 50, 100], 
-              'eta': [0.05, 0.1, 0.4], 
+              'min_child_weight': [0, 0.1, 0.5, 1, 10, 25, 100], 
+              'eta': [opt.eta], 
               'colsample_bytree': [opt.colsample_bytree], 
               'num_boost_round': [200],
               'early_stopping_rounds': [25],
