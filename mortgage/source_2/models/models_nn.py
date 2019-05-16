3c4b9df49470e17a8058e00291b871b1a2d34f4d
Yoann Pradat <pradaty@Yoanns-MacBook-Pro.local>
2019-05-16 21:30:29 +0100

    MLPNet10

--- mortgage/source_2/models/models_nn.py
+++ mortgage/source_2/models/models_nn.py
@@ -148,3 +148,82 @@ class MLPNet5Drop(nn.Module):
         x = self.drop4(F.relu(self.fc4(x)))
         return x
 
+class MLPNet10(nn.Module):
+    def __init__(self, n_input, n_classes):
+        super(MLPNet10, self).__init__()
+        self.n_classes = n_classes
+        self.fc1 = nn.Linear(n_input, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 32)
+        self.fc4 = nn.Linear(32, 32)
+        self.fc5 = nn.Linear(32, 16)
+        self.fc6 = nn.Linear(16, 16)
+        self.fc7 = nn.Linear(16, 16)
+        self.fc8 = nn.Linear(16, 16)
+        self.fc9 = nn.Linear(16, 16)
+        self.fc10 = nn.Linear(16, n_classes)
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = F.relu(self.fc6(x))
+        x = F.relu(self.fc7(x))
+        x = F.relu(self.fc8(x))
+        x = F.relu(self.fc9(x))
+        x = self.fc10(x)
+        return x
+    def get_features(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = F.relu(self.fc6(x))
+        x = F.relu(self.fc7(x))
+        x = F.relu(self.fc8(x))
+        x = F.relu(self.fc9(x))
+        return x
+
+class MLPNet10Drop(nn.Module):
+    def __init__(self, n_input, n_classes, p=0.5):
+        super(MLPNet5, self).__init__()
+        self.n_classes = n_classes
+        self.p = p
+        self.drop = nn.Dropout(self.p)
+        self.fc1 = nn.Linear(n_input, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 32)
+        self.fc4 = nn.Linear(32, 32)
+        self.fc5 = nn.Linear(32, 16)
+        self.fc6 = nn.Linear(16, 16)
+        self.fc7 = nn.Linear(16, 16)
+        self.fc8 = nn.Linear(16, 16)
+        self.fc9 = nn.Linear(16, 16)
+        self.fc10 = nn.Linear(16, n_classes)
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.drop(F.relu(self.fc6(x)))
+        x = self.drop(F.relu(self.fc7(x)))
+        x = self.drop(F.relu(self.fc8(x)))
+        x = self.drop(F.relu(self.fc9(x)))
+        x = self.fc10(x)
+        return x
+    def get_features(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.drop(F.relu(self.fc6(x)))
+        x = self.drop(F.relu(self.fc7(x)))
+        x = self.drop(F.relu(self.fc8(x)))
+        x = self.drop(F.relu(self.fc9(x)))
+        return x
+
+
